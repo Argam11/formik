5aa1af8da4b719365af9c258f4572def757c73be
Aspram <aspram.markosyan7@gmail.com>
2021-06-26 00:33:11 +0400

    add features

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,7 @@ import "./Form.css";
 const validationSchema = Yup.object({
   name: Yup.string().required("Required!"),
   email: Yup.string().email("Invalid email").required("Required!"),
+  ["obj.a"]: Yup.string().required("Required 55"),
 });
 
 const Form = () => {
@@ -16,14 +17,22 @@ const Form = () => {
 
   return (
     <div className="form">
-      <Formik initialValues={{ name: "", email: "" }} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <Formik initialValues={{ name: "", email: "", "obj.a": "" }} validationSchema={validationSchema} onSubmit={onSubmit}>
         {(formik) => (
           <form onSubmit={formik.handleSubmit}>
-            <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} />
-            <ErrorMessage name="name" />
-            <br />
-            <input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} />
-            <ErrorMessage name="email" />
+            <div className="form-control">
+              <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} />
+              <ErrorMessage name="name" />
+            </div>
+            <div className="form-control">
+              <input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} />
+              <ErrorMessage name="email" />
+            </div>
+            <div className="form-control">
+              {console.log(formik.errors)}
+              <input type="text" name="['obj.a']" value={formik.values["obj.a"]} onChange={formik.handleChange} />
+              <ErrorMessage name="['obj.a']" />
+            </div>
             <br />
             <button type="submit">Submit</button>
           </form>
